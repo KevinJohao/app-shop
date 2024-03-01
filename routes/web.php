461164ddcf9b860eeb82de30287d2387e2e0db63
Johao Lema <johaolema@gmail.com>
2024-03-01 09:38:51 -0500

    Base project

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', 'App\Http\Controllers\TestController@welcome');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
